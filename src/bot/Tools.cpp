f67d68eb54fea136397c70ce2765dea97fa10cfc
TinyProbe <137adv167@gmail.com>
2025-05-27 16:08:30 +0900

    Update Changes

--- src/bot/Tools.cpp
+++ src/bot/Tools.cpp
@@ -30,14 +30,11 @@ int CountUnitsOfType(BWAPI::UnitType type, BWAPI::Unitset const &units) {
 }
 
 BWAPI::Unit GetUnitOfType(BWAPI::UnitType type) {
-    // For each unit that we own
     for (auto &unit : BWAPI::Broodwar->self()->getUnits()) {
-        // if the unit is of the correct type, and it actually has been constructed, return it
         if (unit->getType() == type && unit->isCompleted()) {
             return unit;
         }
     }
-    // If we didn't find a valid unit to return, make sure we return nullptr
     return nullptr;
 }
 
@@ -46,20 +43,14 @@ BWAPI::Unit GetDepot() {
     return GetUnitOfType(depot);
 }
 
-// Attempt tp construct a building of a given type 
 bool BuildBuilding(BWAPI::UnitType type) {
-    // Get the type of unit that is required to build the desired building
     BWAPI::UnitType builderType = type.whatBuilds().first;
 
-    // Get a unit that we own that is of the given type so it can build
-    // If we can't find a valid builder unit, then we have to cancel the building
     BWAPI::Unit builder = Tools::GetUnitOfType(builderType);
     if (!builder) { return false; }
 
-    // Get a location that we want to build the building next to
     BWAPI::TilePosition desiredPos = BWAPI::Broodwar->self()->getStartLocation();
 
-    // Ask BWAPI for a building location near the desired position for the type
     int maxBuildRange = 64;
     bool buildingOnCreep = type.requiresCreep();
     BWAPI::TilePosition buildPos = BWAPI::Broodwar->getBuildLocation(type, desiredPos, maxBuildRange, buildingOnCreep);
@@ -70,20 +61,14 @@ void DrawUnitCommands() {
     for (auto &unit : BWAPI::Broodwar->self()->getUnits()) {
         BWAPI::UnitCommand const &command = unit->getLastCommand();
 
-        // If the previous command had a ground position target, draw it in red
-        // Example: move to location on the map
         if (command.getTargetPosition() != BWAPI::Positions::None) {
             BWAPI::Broodwar->drawLineMap(unit->getPosition(), command.getTargetPosition(), BWAPI::Colors::Red);
         }
 
-        // If the previous command had a tile position target, draw it in red
-        // Example: build at given tile position location
         if (command.getTargetTilePosition() != BWAPI::TilePositions::None) {
             BWAPI::Broodwar->drawLineMap(unit->getPosition(), BWAPI::Position(command.getTargetTilePosition()), BWAPI::Colors::Green);
         }
 
-        // If the previous command had a unit target, draw it in red
-        // Example: attack unit, mine mineral, etc
         if (command.getTarget() != nullptr) {
             BWAPI::Broodwar->drawLineMap(unit->getPosition(), command.getTarget()->getPosition(), BWAPI::Colors::White);
         }
@@ -99,76 +84,56 @@ void DrawUnitBoundingBoxes() {
 }
 
 void SmartRightClick(BWAPI::Unit unit, BWAPI::Unit target) {
-    // if there's no valid unit, ignore the command
     if (!unit || !target) { return; }
 
-    // Don't issue a 2nd command to the unit on the same frame
     if (unit->getLastCommandFrame() >= BWAPI::Broodwar->getFrameCount()) { return; }
 
-    // If we are issuing the same type of command with the same arguments, we can ignore it
-    // Issuing multiple identical commands on successive frames can lead to bugs
     if (unit->getLastCommand().getTarget() == target) { return; }
 
-    // If there's nothing left to stop us, right click!
     unit->rightClick(target);
 }
 
 int GetTotalSupply(bool inProgress) {
-    // start the calculation by looking at our current completed supplyt
     int totalSupply = BWAPI::Broodwar->self()->supplyTotal();
 
-    // if we don't want to calculate the supply in progress, just return that value
     if (!inProgress) { return totalSupply; }
 
-    // if we do care about supply in progress, check all the currently constructing units if they will add supply
     for (auto &unit : BWAPI::Broodwar->self()->getUnits()) {
-        // ignore units that are fully completed
         if (unit->isCompleted()) { continue; }
 
-        // if they are not completed, then add their supply provided to the total supply
         totalSupply += unit->getType().supplyProvided();
     }
 
-    // one last tricky case: if a unit is currently on its way to build a supply provider, add it
     for (auto &unit : BWAPI::Broodwar->self()->getUnits()) {
-        // get the last command given to the unit
         BWAPI::UnitCommand const &command = unit->getLastCommand();
 
-        // if it's not a build command we can ignore it
         if (command.getType() != BWAPI::UnitCommandTypes::Build) { continue; }
 
-        // add the supply amount of the unit that it's trying to build
         totalSupply += command.getUnitType().supplyProvided();
     }
     return totalSupply;
 }
 
 void DrawUnitHealthBars() {
-    // how far up from the unit to draw the health bar
     int verticalOffset = -10;
 
-    // draw a health bar for each unit on the map
     for (auto &unit : BWAPI::Broodwar->getAllUnits()) {
-        // determine the position and dimensions of the unit
         BWAPI::Position const &pos = unit->getPosition();
         int left = pos.x - unit->getType().dimensionLeft();
         int right = pos.x + unit->getType().dimensionRight();
         int top = pos.y - unit->getType().dimensionUp();
         int bottom = pos.y + unit->getType().dimensionDown();
 
-        // if it's a resource, draw the resources remaining
         if (unit->getType().isResourceContainer() && unit->getInitialResources() > 0) {
             double mineralRatio = (double)unit->getResources() / (double)unit->getInitialResources();
             DrawHealthBar(unit, mineralRatio, BWAPI::Colors::Cyan, 0);
         } else if (unit->getType().maxHitPoints() > 0) {
-            // otherwise if it's a unit, draw the hp 
             double hpRatio = (double)unit->getHitPoints() / (double)unit->getType().maxHitPoints();
             BWAPI::Color hpColor = BWAPI::Colors::Green;
             if (hpRatio < 0.66) hpColor = BWAPI::Colors::Orange;
             if (hpRatio < 0.33) hpColor = BWAPI::Colors::Red;
             DrawHealthBar(unit, hpRatio, hpColor, 0);
 
-            // if it has shields, draw those too
             if (unit->getType().maxShields() > 0) {
                 double shieldRatio = (double)unit->getShields() / (double)unit->getType().maxShields();
                 DrawHealthBar(unit, shieldRatio, BWAPI::Colors::Blue, -3);
